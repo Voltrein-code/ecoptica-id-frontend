9b26c9fb0853e2602cfcec4fd7922e0ef3455713
Voltrein-code <mr-votlrein@yandex.ru>
2023-06-08 23:08:38 +0300

    feat: Добавить начисления бонусов

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,6 +8,9 @@ import Countdown from "react-countdown";
 import preloaderSVG from "../../media/puff-black.svg";
 import shopCard from "../../media/shop-card.svg";
 
+import debitIcon from "../../media/alt-plus-frame-svgrepo-com.svg";
+import creditIcon from "../../media/alt-minus-frame-svgrepo-com.svg";
+
 export default function Shop() {
   const [cardShow, setCardShow] = useState(false);
   const [cardLoading, setCardLoading] = useState(false);
@@ -58,6 +61,63 @@ export default function Shop() {
               value="46020080663921620366659511819655767220348495277"
             />
           )}
+        </div>
+        <div className="home__notifications">
+          <h3 className="home__notifications-heading">История операций</h3>
+          <ul className="home__notifications-container">
+            <li className="home__notifications-item">
+              <img
+                className="home__notifications-priority"
+                alt="Приоритет"
+                src={debitIcon}
+              ></img>
+              <div className="home__notifications-text">
+                <h5 className="home__notifications-title">
+                  Начисление: 115 баллов
+                </h5>
+                <p className="home__notifications-date">1 июня 2023 • 12:01</p>
+              </div>
+            </li>
+            <li className="home__notifications-item">
+              <img
+                className="home__notifications-priority"
+                alt="Приоритет"
+                src={debitIcon}
+              ></img>
+              <div className="home__notifications-text">
+                <h5 className="home__notifications-title">
+                  Начисление: 400 баллов
+                </h5>
+                <p className="home__notifications-date">30 мая 2023 • 12:20</p>
+              </div>
+            </li>
+            <li className="home__notifications-item">
+              <img
+                className="home__notifications-priority"
+                alt="Приоритет"
+                src={creditIcon}
+              ></img>
+              <div className="home__notifications-text">
+                <h5 className="home__notifications-title">
+                  Списание: 174 балла
+                </h5>
+                <p className="home__notifications-date">29 мая 2023 • 9:01</p>
+              </div>
+            </li>
+            <li className="home__notifications-item">
+              <img
+                className="home__notifications-priority"
+                alt="Приоритет"
+                src={creditIcon}
+              ></img>
+              <div className="home__notifications-text">
+                <h5 className="home__notifications-title">
+                  Списание: 5 баллов
+                </h5>
+                <p className="home__notifications-date">28 мая 2023 • 12:01</p>
+              </div>
+            </li>
+          </ul>
         </div>
         <button
           disabled={cardShow}
